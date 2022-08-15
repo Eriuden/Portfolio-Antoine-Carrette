b7d5f54992c40b7407fdc51d9465bc0f45303695
Eriuden <antoinecarrette@hotmail.fr>
2022-08-15 22:28:06 +0200

    update15august

--- src/Containers/Creations.js
+++ src/Containers/Creations.js
@@ -6,6 +6,10 @@ export default function Creations() {
   const [toggleCréations, setToggleCréations] = useState(false);
   const [toggleCréations2, setToggleCréations2] = useState(false);
   const [toggleCréations3, setToggleCréations3] = useState(false);
+  const [toggleCréations4, setToggleCréations4] = useState(false);
+  const [toggleCréations5, setToggleCréations5] = useState(false);
+  const [toggleCréations6, setToggleCréations6] = useState(false);
+  
   return (
     <div className="creations">
       <h2 className="NomProjet">Creations/experiences</h2>
@@ -120,6 +124,84 @@ export default function Creations() {
           )}
         </p>
       </div>
+
+      <div className="Projet">
+        <h3 className="NomProjet">Trouve ton toit</h3>
+
+        <p
+          className="scrollP"
+          onClick={() => setToggleCréations4(!toggleCréations4)}
+        >
+          Un hook Usestate me cache, cliquez sur moi
+          {toggleCréations4 && (
+            <>
+              <p className="ProjetP">
+                Un projet en équipe, plus précisément, pour un Hackathon, à
+                savoir que nous avons terminé sur le podium (3ème). Le projet
+                fut réalisé à l'aide de RedwoodJS et tailwind, et eut l'intérêt
+                d'améliorer mes softskills de travail en équipe
+              </p>
+
+              <p className="ProjetP">
+                Il s'agissait de créer une plateforme permettant la recherche
+                d'emploi et de logements. L'idée finale était de relier les
+                deux, au sens où; si l'on consulte un logement, les emplois
+                proches s'afficheront, et inversement.
+              </p>
+            </>
+          )}
+        </p>
+      </div>
+
+      <div className="Projet">
+        <h3 className="NomProjet">Rent a bike</h3>
+
+        <p
+          className="scrollP"
+          onClick={() => setToggleCréations5(!toggleCréations5)}
+        >
+          Un hook Usestate me cache, cliquez sur moi
+          {toggleCréations5 && (
+            <>
+              <p className="ProjetP">
+                Projet plus classique d'e-commerce, ici, il s'agit, comme l'indique le nom, d'un équivalent deux routes de rent a car,
+                on retrouve donc les fondamentaux de MERN
+              </p>
+
+              <p className="ProjetP">
+                Ici, il s'agit, comme l'indique le nom, d'un équivalent deux roues de rent a car, chaque motos est affiché selon la liste sur 
+                la page d'acceuil, puis, dispose de sa propre Page, à l'heure actuelle, du au manque de connaissance là dessus, j'ai préféré ne pas
+                traiter la partie du site qui touche aux opérations monétaires.
+              </p>
+            </>
+          )}
+        </p>
+      </div>
+
+      <div className="Projet">
+        <h3 className="NomProjet">Travel Low</h3>
+
+        <p
+          className="scrollP"
+          onClick={() => setToggleCréations6(!toggleCréations6)}
+        >
+          Un hook Usestate me cache, cliquez sur moi
+          {toggleCréations6 && (
+            <>
+              <p className="ProjetP">
+                On revient à une idée de réseau social en MERN, ici thématique, à propos des voyages
+              </p>
+
+              <p className="ProjetP">
+                Comme dit au dessus, un réseau social, les utilisateurs peuvent créer des posts à props de destination/voyages,
+                avec un nom, des photos, et une description libre (les prix, conseil de lieux à visiter, etc...).
+                Il n'est pas question ici d'une agence de voyages, c'est vraiment un réseau social pour partager ses bons plans et expériences.
+              </p>
+            </>
+          )}
+        </p>
+      </div>
+
     </div>
   );
 }
